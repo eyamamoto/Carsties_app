cadb4a764d915ed8f1933c1ce77e6480f52652f0
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-09-18 18:12:46 -0300

    Cap3 - Criação do microservice de Search

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,29 +1,14 @@
-using AuctionService.Data;
-using AuctionService.Data.Seed;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
-
-//database
-builder.Services.AddDbContext<AuctionDbContext>(opt =>
-{
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-//mapper
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -40,15 +25,5 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
-//executa o seeder
-try
-{
-    DbInitializer.InitiDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.ToString());
-}
 
 app.Run();
