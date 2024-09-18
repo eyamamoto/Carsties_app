bf8fa42e3392dbd5b5c27c91af7c2078489c29b4
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-09-18 20:45:08 -0300

    Cap3 - Adicionando controller para o Search Microservice

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SearchService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+        {
+            //busca os itens e ordena pelo campo Make
+            var query = DB.Find<Item>();
+            query.Sort(x => x.Ascending(a => a.Make));
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                query.Match(Search.Full, searchTerm).SortByTextScore();
+            }
+
+            var result = await query.ExecuteAsync();
+
+            return result;
+        }
+    }
+}
