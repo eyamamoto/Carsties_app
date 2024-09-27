8ce8dac429c28e2ade8b08246a8343f49dba637a
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-09-27 18:13:25 -0300

    Cap6 - Adicionando os consumers finais no projeto de Search

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+using System.Threading.Tasks;
+
+namespace SearchService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFInished>
+    {
+        public async Task Consume(ConsumeContext<AuctionFInished> context)
+        {
+            var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+            if (context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = (int)context.Message.Amount;
+            }
+
+            auction.Status = "Finished";
+
+            await auction.SaveAsync();
+        }
+    }
+}
