d4f34e7788fa3e38efdc03ecc91352ff31c81d17
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-09-29 14:16:48 -0300

    Testes - Criando Repositorio de Auctions

---
+++ src/AuctionService/Data/Repository/AuctionRepository.cs
@@ -0,0 +1,63 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionService.Data.Repository
+{
+    public class AuctionRepository : IAuctionRepository
+    {
+        private readonly AuctionDbContext context;
+        private readonly IMapper mapper;
+
+        public AuctionRepository(AuctionDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+        public void AddAuction(Auction auction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AuctionDto> GetAuctionByIdAsync(Guid id)
+        {
+            return await context.Auctions
+               .ProjectTo<AuctionDto>(mapper.ConfigurationProvider)
+               .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Auction> GetAuctionEntityById(Guid id)
+        {
+            return await context.Auctions.Include(x => x.Item)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<AuctionDto>> GetAuctionsAsync(string date)
+        {
+            var query = context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
+
+            if (!string.IsNullOrEmpty(date))
+            {
+                query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+            }
+
+            return await query.ProjectTo<AuctionDto>(mapper.ConfigurationProvider).ToListAsync();
+        }
+
+        public void RemoveAuction(Auction auction)
+        {
+            context.Auctions.Remove(auction);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ src/AuctionService/Data/Repository/IAuctionRepository.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AuctionService.Data.Repository
+{
+    public interface IAuctionRepository
+    {
+        Task<List<AuctionDto>> GetAuctionsAsync(string date);
+        Task<AuctionDto> GetAuctionByIdAsync(Guid id);
+        Task<Auction> GetAuctionEntityById(Guid id);
+        void AddAuction(Auction auction);
+        void RemoveAuction(Auction auction);
+        Task<bool>SaveChangesAsync();
+    }
+}
