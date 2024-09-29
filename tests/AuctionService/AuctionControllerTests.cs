b14af25806caccc1d8ac0b11c8f238178f2e886d
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-09-29 15:50:27 -0300

    Testes - Criando primeiro caso de testes

--- tests/AuctionService/AuctionControllerTests.cs
+++ tests/AuctionService/AuctionControllerTests.cs
@@ -1,9 +1,11 @@
 ﻿using AuctionService.Controllers;
 using AuctionService.Data.Repository;
+using AuctionService.DTOs;
 using AuctionService.Mappers;
 using AutoFixture;
 using AutoMapper;
 using MassTransit;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -34,6 +36,24 @@ namespace AuctionService.UnitTest
 
             _mapper = new Mapper(mockMapper);
             _controller = new AuctionsController(_auctionRepo.Object, _mapper,_publishEndpoint.Object);
+        }
+
+        [Fact]
+        public async Task GetAuctions_WithNoParams_Returns10Auctions()
+        {
+            //arrange
+            //cria 10 auctionDto e guarda na variavel
+            var auctions = _fixture.CreateMany<AuctionDto>(10).ToList();
+            //cria o mock do retorno da função do repositorio
+            _auctionRepo.Setup(repo => repo.GetAuctionsAsync(null)).ReturnsAsync(auctions);
+
+            //act
+            //chama o metodo do controller
+            var result = await _controller.GetAllAuctions(null);
+
+            //assert
+            Assert.Equal(10, result.Value.Count);
+            Assert.IsType<ActionResult<List<AuctionDto>>>(result);
         }
     }
 }
