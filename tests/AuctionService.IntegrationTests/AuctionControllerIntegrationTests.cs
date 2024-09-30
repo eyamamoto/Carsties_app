503f6cba2bec129fdd72d701a9d2da962fdbeb99
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-09-30 00:16:09 -0300

    Testes Integrados - Executando outros casos de testes integrados no controller Auction

--- tests/AuctionService.IntegrationTests/AuctionControllerIntegrationTests.cs
+++ tests/AuctionService.IntegrationTests/AuctionControllerIntegrationTests.cs
@@ -3,6 +3,7 @@ using AuctionService.DTOs;
 using AuctionService.IntegrationTests.Fixtures;
 using AuctionService.IntegrationTests.Util;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net;
 using System.Net.Http.Json;
 
 namespace AuctionService.IntegrationTests
@@ -11,6 +12,8 @@ namespace AuctionService.IntegrationTests
     {
         private readonly CustomWebAppFactory factory;
         private readonly HttpClient httpclient;
+        private const string Model_T_ID = "3659ac24-29dd-407a-81f5-ecfe6f924b9b";
+
         //a cada teste o banco de dados deve ser limpo e reiniciado
 
         public AuctionControllerIntegrationTests(CustomWebAppFactory factory)
@@ -31,6 +34,41 @@ namespace AuctionService.IntegrationTests
             Assert.Equal(10, response.Count());
         }
 
+        [Fact]
+        public async Task GetAuctionById_WithValidId_ShouldReturnAuction()
+        {
+            //arrange
+
+            //act
+            var response = await httpclient.GetFromJsonAsync<AuctionDto>($"api/auctions/{Model_T_ID}");
+
+            //assert
+            Assert.Equal("Model T", response.Model);
+        }
+
+        [Fact]
+        public async Task GetAuctionById_WithInvalidId_ShouldReturn404()
+        {
+            //arrange
+
+            //act
+            var response = await httpclient.GetAsync($"api/auctions/{Guid.NewGuid()}");
+
+            //assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAuctionById_WithInvalidIdGuid_ShouldReturn400()
+        {
+            //arrange
+
+            //act
+            var response = await httpclient.GetAsync($"api/auctions/notAGUID");
+
+            //assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
 
         //antes de cada teste esse metodo é executado
         public Task InitializeAsync()
