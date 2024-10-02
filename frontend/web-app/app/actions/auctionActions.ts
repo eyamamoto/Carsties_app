c8c005b4766d9774a3c98e16316d8f2cfe4ab47b
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-02 12:43:24 -0300

    FrontEnd - REfatorando codigo para usar o zustand

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -3,8 +3,8 @@
 import { Auction, PagedResult } from "../types";
 
 //carregar dados
-export async function getData(pageNumber:number, pageSize:number): Promise<PagedResult<Auction>>{
-    const res = await fetch( `http://localhost:6001/search?pagesize=${pageSize}&pageNumber=${pageNumber}` );
+export async function getData(query:string): Promise<PagedResult<Auction>>{
+    const res = await fetch( `http://localhost:6001/search${query}` );
 
     if(!res.ok) throw new Error('Fail to fetch error');
 
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -4,37 +4,56 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import AuctionCard from './AuctionCard';
 import AppPagination from '../components/AppPagination';
-import { Auction } from '../types';
+import { Auction, PagedResult } from '../types';
 import { getData } from '../actions/auctionActions';
 import Filters from './Filters';
-
+import { useParamsStore } from '../hooks/useParamsStore';
+import { useShallow } from 'zustand/shallow';
+import qs from 'query-string'
 
 export default function Listings() {
-    const [auctions, setAuctions] = useState<Auction[]>([]);
-    const [pageCount, setPageCount] = useState(0);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [pageSize, setPagesize] = useState(4);
-    useEffect(() =>{
-        getData(pageNumber, pageSize).then(data =>{
-            setAuctions(data.results);
-            setPageCount(data.pageCount)
-        })
-    },[pageNumber, pageSize])
+    // const [auctions, setAuctions] = useState<Auction[]>([]);
+    // const [pageCount, setPageCount] = useState(0);
+    // const [pageNumber, setPageNumber] = useState(1);
+    // const [pageSize, setPagesize] = useState(4);
 
-    if(auctions.length === 0) return <h3>Loading ... </h3>
+    const [data, setData] = useState<PagedResult<Auction>>();
+
+    const params = useParamsStore(
+        useShallow( 
+            state => ({
+                pageNumber:state.pageNumber,
+                pageSize:state.pageSize,
+                searchTherm:state.searchTerm
+            })
+        )
+    );
+
+    const setParams = useParamsStore(state => state.steParams);
+    const url = qs.stringifyUrl({url:'', query:params})
+
+    function setPageNumber(pageNumber:number){
+        setParams({pageNumber})
+    }
+    
+    useEffect(() =>{
+        getData(url).then(data =>{
+            setData(data)
+        })
+    },[url])
+
+    if(!data) return <h3>Loading ... </h3>
 
     return (
         <Fragment>
-            <Filters pageSize={pageSize} setPageSize={setPagesize}
-
-            />
+            <Filters />
             <div className='grid grid-cols-4 gap-5'>
-                {auctions.map((auction) => (
+                {data.results.map((auction) => (
                     <AuctionCard key={auction.id} auction={auction} />
                 ))}
             </div>
             <div className='flex justify-center mt-4'>
-                <AppPagination pageChanged={setPageNumber} currentPage={pageNumber} pageCount={pageCount}/>
+                <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount}/>
             </div>
         </Fragment>
     )
