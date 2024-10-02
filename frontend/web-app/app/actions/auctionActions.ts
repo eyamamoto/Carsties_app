572d92f58c21aa01c9ab09b29c1281ad8737f95c
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-01 21:51:21 -0300

    FrontEnd - Adicionando opção de setPageSize  na lista de leilões

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -3,8 +3,8 @@
 import { Auction, PagedResult } from "../types";
 
 //carregar dados
-export async function getData(pageNumber:number = 1): Promise<PagedResult<Auction>>{
-    const res = await fetch( `http://localhost:6001/search?pagesize=10&pageNumber=${pageNumber}` );
+export async function getData(pageNumber:number, pageSize:number): Promise<PagedResult<Auction>>{
+    const res = await fetch( `http://localhost:6001/search?pagesize=${pageSize}&pageNumber=${pageNumber}` );
 
     if(!res.ok) throw new Error('Fail to fetch error');
 
