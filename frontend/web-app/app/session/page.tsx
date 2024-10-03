d3713734e8a38d27a1ab61ecff8de1da7f2b9fe6
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-02 23:36:06 -0300

    FrontEnd Auth - Recuperando detalhes da sessão no cliente nextjs

---
+++ frontend/web-app/app/session/page.tsx
@@ -0,0 +1,19 @@
+import { auth } from '@/auth'
+import React from 'react'
+import Heading from '../components/Heading';
+
+export default async function Session() {
+    const session = await auth();
+
+    return (
+        <div>
+            <Heading title='Session dashboard' subtitle={''}/>
+            <div className='bg-blue-200 border-2 border-blue-500'>
+                <h3 className='text-lg'>session Data</h3>
+                <pre>
+                    {JSON.stringify(session, null, 2)}
+                </pre>
+            </div>
+        </div>
+    )
+}
