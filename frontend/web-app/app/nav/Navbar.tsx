ffc677a3689cf689676dcfd4531c99c1e289a271
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-03 00:37:04 -0300

    FrontEnd Auth - Populando o dropdown de usuarios com dados da sessão e adicionando componente Logo

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,24 +1,23 @@
 
 import React from 'react'
-import { AiOutlineCar } from 'react-icons/ai'
 import Search from './Search'
 import LoginButton from './LoginButton'
 import { getCurrentUser } from '../actions/authActions'
 import UserActions from './UserActions'
+import Logo from './Logo'
 
 export default async function Navbar() {
   const user = await getCurrentUser();
-
+ 
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
-        <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
-            <AiOutlineCar size={34}/>
-            <div>Leilões</div>
-        </div>
+        
+        <Logo/>
+
         <Search/>
 
         {user ? (
-          <UserActions/>
+          <UserActions user={user}/>
         ) :(
           <LoginButton/>
         )}
--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,11 +1,47 @@
-import { Button } from 'flowbite-react'
-import Link from 'next/link'
-import React from 'react'
+'use client'
 
-export default function UserActions() {
-  return (
-    <Button outline>
-        <Link href="/session">Session</Link>
-    </Button>
-  )
+import {  Dropdown, DropdownDivider, DropdownItem } from 'flowbite-react'
+import { User } from 'next-auth'
+import { signOut } from 'next-auth/react'
+import Link from 'next/link'
+//import { useRouter } from 'next/router'
+import React from 'react'
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
+import { HiCog, HiUser } from 'react-icons/hi'
+
+type Props = {
+    user: User
+}
+
+export default function UserActions({user}:Props) {
+    //const router = useRouter();
+
+    return (
+        <Dropdown inline label={`Welcome ${user.name}` }>
+            <DropdownItem icon={HiUser}>
+                <Link href="/">
+                    My Auctions
+                </Link>
+            </DropdownItem>
+            <DropdownItem icon={AiFillTrophy}>
+                <Link href="/">
+                    Auction won
+                </Link>
+            </DropdownItem>
+            <DropdownItem icon={AiFillCar}>
+                <Link href="/">
+                    Sell my car
+                </Link>
+            </DropdownItem>
+            <DropdownItem icon={HiCog}>
+                <Link href="/session">
+                    Session (development only !)
+                </Link>
+            </DropdownItem>
+            <DropdownDivider/>
+            <DropdownItem icon={AiOutlineLogout} onClick={() => signOut({callbackUrl:'/'})}>
+                Sign Out
+            </DropdownItem>
+        </Dropdown>
+    )
 }
