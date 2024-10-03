f3b8c5c6032c61ab03e9e8a2b8f0fce589a2d818
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-03 17:48:23 -0300

    FrontEnd Routes - Iniciando com rotas no nextJS

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import CountdownTimer from './CountdownTimer'
 import CarImage from './CarImage'
 import { Auction } from '../types'
+import Link from 'next/link'
 
 //props
 type Props = {
@@ -13,7 +14,7 @@ type Props = {
 
 export default function AuctionCard({auction}: Props) {
   return (
-    <a href='#' className='group'>
+    <Link href={`/auctions/details/${auction.id}`} className='group'>
         <div className=' relative w-full bg-gray-200 aspect-[16/10] rounded-lg overflow-hidden'>
             
             <CarImage auction={auction} />
@@ -21,11 +22,14 @@ export default function AuctionCard({auction}: Props) {
             <div className='absolute bottom-2 left-2'>
                 <CountdownTimer auctionEnd={auction.auctionEnd} />
             </div>
+
         </div>
+
         <div className='flex justify-between items-center mt-4'>
             <h3 className='text-gray-700'>{auction.make} {auction.model}</h3>
             <p className='font-semibold, text-sm'>{auction.year}</p>
         </div>
-    </a>
+
+    </Link>
   )
 }
