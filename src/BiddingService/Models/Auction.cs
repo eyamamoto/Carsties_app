755611ba1ecdb1ef11e5c7439de1a5bcda68cc2f
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-04 14:23:35 -0300

    BidServices - Criando os models de bid

---
+++ src/BiddingService/Models/Auction.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Entities;
+using System;
+
+namespace BiddingService.Models
+{
+    public class Auction : Entity
+    {
+        public DateTime AuctionEnd { get; set; }
+        public string Seller { get; set; }
+        public int ReservePrice { get; set; }
+        public bool Finished { get; set; }
+
+    }
+}
