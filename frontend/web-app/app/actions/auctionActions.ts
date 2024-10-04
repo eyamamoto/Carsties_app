06aa4a6a157a5e8195cfe182a98b76643b7a356b
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-03 23:07:52 -0300

    FrontEnd Routes - Formularios - criando tela de detalhes de leilão

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -15,12 +15,15 @@ export async function updateAuctionTest(){
     const data = {
         mileage: Math.floor(Math.random() * 10000) + 1
     }
-
     return await fetchWrapper.put('auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', data);
 }
 
 export async function createAuction(data:FieldValues){
     return await fetchWrapper.post('auctions',data);
+}
+
+export async function getDetailedViewData(id:string): Promise<Auction>{
+    return await fetchWrapper.get(`auctions/${id}`);
 }
 
 //auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c
