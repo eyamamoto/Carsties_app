3e69ffc2616cb3ce12bdd1af50f4e851d412b499
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-13 23:50:03 -0300

    FrontEnd - Adicionando condicionais na tela de lances de leilão

--- frontend/web-app/app/auctions/details/[id]/BidList.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidList.tsx
@@ -40,28 +40,38 @@ export default function BidList({user, auction}:Props) {
     if(loading) return <div> Loading... </div>
 
     return (
-        <>
-            <div className='rounded-lg shadow-md'>
-                <div className='py-2 px-4 bg-white'>
-                    <div className='sticky top-0 bg-white p-2'>
-                        <Heading title={`current high bid is ${numberWithCommas(highBid)}`} subtitle={''} />
-                    </div>
-                </div>
-                <div className='overflow-auto h-[400px] flex flex-col-reverse px-2'>
-                    {bids.length === 0 ? (
-                        <EmptyFilter title='no bid' subtitle='please fell free to make a bid'/>
-                    ): (
-                        <>
-                            {bids.map(bid => (
-                                <BidItem key={bid.id} bid={bid} />
-                            ))}
-                        </>
-                    )}
-                </div>
-                <div className='px-2 pb-2 text-gray-500'>
-                    <BidForm auctionId={auction.id} highBid={highBid}/>
+
+        <div className='rounded-lg shadow-md'>
+            <div className='py-2 px-4 bg-white'>
+                <div className='sticky top-0 bg-white p-2'>
+                    <Heading title={`current high bid is ${numberWithCommas(highBid)}`} subtitle={''} />
                 </div>
             </div>
-        </>
+            <div className='overflow-auto h-[400px] flex flex-col-reverse px-2'>
+                {bids.length === 0 ? (
+                    <EmptyFilter title='no bid' subtitle='please fell free to make a bid'/>
+                ): (
+                    <>
+                        {bids.map(bid => (
+                            <BidItem key={bid.id} bid={bid} />
+                        ))}
+                    </>
+                )}
+            </div>
+            <div className='px-2 pb-2 text-gray-500'>
+                {!user ? (
+                    <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                        Please login to make a bid
+                    </div>
+                ) : user && user.username === auction.seller ? (
+                    <div className='flex items-center justify-center p-2 text-lg font-semibold'>
+                        You cannot bid on your own auction
+                    </div>
+                ) : (
+                    <BidForm auctionId={auction.id} highBid={highBid} />
+                )}
+            </div>
+        </div>
     )
 }
+
